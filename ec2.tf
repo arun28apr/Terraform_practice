0028cabae04e466d366cc0251872b9ee800e74df
Arun <arun28apr@gmail.com>
2026-01-08 10:44:29 +0530

    3 instances added

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,39 @@
-resource "aws_instance" "web1" {
-  ami = "ami-00ca570c1b6d79f36"
+resource "aws_instance" "Ubuntu_OS" {
+  ami = "ami-02b8269d5e85954ef"
   instance_type = "t2.micro"
   key_name = "Keypair"
+  subnet_id = "subnet-0900d66437acb58ea"
   associate_public_ip_address = "true"
-  vpc_security_group_ids = [ "sg-05453214b489d08b0" ]
+  vpc_security_group_ids = ["sg-05453214b489d08b0"]
 
 tags = {
-  Name = "Web1"
+  Name = "Ubuntu_OS"
 }
-  
+}
+
+resource "aws_instance" "Aws_OS" {
+  ami = "ami-0ced6a024bb18ff2e"
+  instance_type = "t2.micro"
+  key_name = "Keypair"
+  availability_zone = "ap-south-1a"
+  subnet_id = "subnet-05bb039beb76b1835"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = ["sg-05453214b489d08b0"]
+
+tags = {
+  Name = "Aws_OS"
+}
+}
+
+resource "aws_instance" "Redhat_OS" {
+  ami = "ami-01ca13db604661046"
+  instance_type = "t2.micro"
+  key_name = "Keypair"
+  subnet_id = "subnet-05bb039beb76b1835"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = ["sg-05453214b489d08b0"]
+
+tags = {
+  Name = "Redhat_OS"
+}  
 }
