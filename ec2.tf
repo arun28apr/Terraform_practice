8a2a361897e9e0458fec86d17b6e361dc9574b85
Arun <arun28apr@gmail.com>
2026-01-12 10:41:26 +0530

    ec2 and variable modified

--- ec2.tf
+++ ec2.tf
@@ -1,39 +1,36 @@
-resource "aws_instance" "Ubuntu_OS" {
-  ami = "ami-02b8269d5e85954ef"
+resource "aws_instance" "web1" {
+  ami = var.aws_os_2023
   instance_type = var.my_instance_type
   key_name = "Keypair"
-  subnet_id = var.ubuntu_subnet
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = ["sg-05453214b489d08b0"]
+  subnet_id = var.aws_subnet
+  associate_public_ip_address = true
+  vpc_security_group_ids = [ "sg-05453214b489d08b0" ]
+tags = {
+  Name = "AWS_OS"
+}
+}
 
+resource "aws_instance" "web2" {
+  ami = var.ubuntu_os
+  instance_type = var.my_instance_type
+  key_name = "Keypair"
+  subnet_id = var.aws_subnet
+  associate_public_ip_address = true
+  vpc_security_group_ids = [ "sg-05453214b489d08b0" ]
 tags = {
   Name = "Ubuntu_OS"
 }
+  
 }
 
-resource "aws_instance" "Aws_OS" {
-  ami = "ami-0ced6a024bb18ff2e"
-  instance_type = var.my_instance_type
-  key_name = "Keypair"
-  availability_zone = "ap-south-1a"
-  subnet_id = var.aws_subnet
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = ["sg-05453214b489d08b0"]
-
-tags = {
-  Name = "Aws_OS"
-}
-}
-
-resource "aws_instance" "Redhat_OS" {
-  ami = "ami-01ca13db604661046"
+resource "aws_instance" "web3" {
+  ami = var.Redhat_os
   instance_type = var.my_instance_type
   key_name = "Keypair"
   subnet_id = var.Redhat_subnet
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = ["sg-05453214b489d08b0"]
-
+  associate_public_ip_address = true
+  vpc_security_group_ids = [ "sg-05453214b489d08b0" ]
 tags = {
   Name = "Redhat_OS"
-}  
+}
 }
--- variable.tf
+++ variable.tf
@@ -1,22 +1,36 @@
-variable "my_instance_type" {
-    description = "Free tier t2.micro"
+variable "aws_os_2023" {
+    description = "AWS OS for the server"
     type = string
-    default = "t2.micro"  
+    default = "ami-0ced6a024bb18ff2e"
 }
-variable "ubuntu_subnet" {
-    description = "subnet id for ubuntu"
+
+variable "my_instance_type" {
+    description = "Free tier instance"
+    type = string
+    default = "t2.micro"
+}
+
+variable "aws_subnet" {
+    description = "Subnet id for aws os"
     type = string
     default = "subnet-0900d66437acb58ea"
-  
+    
 }
-variable "aws_subnet" {
-    description = "subnet id for aws"
+
+variable "ubuntu_os" {
+    description = "Ubuntu OS for the instance"
+    default = "ami-02b8269d5e85954ef"
     type = string
-    default = "subnet-05bb039beb76b1835"
-  
 }
+
+variable "Redhat_os" {
+    description = "Redhat OS for instance"
+    default = "ami-0e57fa2ecfa799574"
+    type = string
+}
+
 variable "Redhat_subnet" {
-    description = "subnet id for redhat"
-    type = string
+    description = "Subent id for Redhat"
     default = "subnet-05bb039beb76b1835"
-  }
+    type = string
+}
